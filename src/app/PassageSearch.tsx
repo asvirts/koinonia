835a2df619c109ff38f4477bd146b0cefd58e877
Andrew Virts <asvirts@outlook.com>
2025-03-06 15:23:59 -0600

    returns as json now

--- src/app/PassageSearch.tsx
+++ src/app/PassageSearch.tsx
@@ -1,6 +1,9 @@
 import OpenAI from "openai"
 
-export default async function PassageSearch() {
+export default async function PassageSearch(props: {
+  search: string
+  loading: boolean
+}) {
   const openai = new OpenAI({ apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY })
 
   const verses = "Galatians 2"
@@ -12,21 +15,20 @@ export default async function PassageSearch() {
       {
         role: "system",
         content:
-          "You are a Christian Biblical scholar. Provide a small group discussion guide for the given passages of Scripture."
+          "You are a Christian Biblical scholar. Provide a small group discussion guide for the given passages of Scripture. Only reply with the questions in valid JSON format."
       },
       {
         role: "user",
         content: `Create ${questions} small group discussion questions based on ${verses}`
       }
     ],
+    response_format: { type: "json_object" },
     store: true
   })
 
-  console.log(completion.choices[0].message)
-
-  return (
-    <div>
-      <p>{completion.choices[0].message.content}</p>
-    </div>
+  return props.loading ? (
+    <div>Loading...</div>
+  ) : (
+    <div>{completion.choices[0].message.content}</div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+"use client"
+
+import { useState } from "react"
 import PassageSearch from "./PassageSearch"
 
 export default function Home() {
+  const [search, setSearch] = useState("")
+  const [loading, setLoading] = useState(false)
+
   return (
     <main>
       <h1>Home</h1>
-      <PassageSearch />
+      <input type="text" onChange={(e) => setSearch(e.target.value)} />
+      <PassageSearch search={search} loading={loading} />
     </main>
   )
 }
