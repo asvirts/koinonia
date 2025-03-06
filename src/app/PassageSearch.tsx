af817d0b9ca0d5c4b68d3fdbee5ea5d28750a9be
Andrew Virts <asvirts@outlook.com>
2025-03-06 14:51:18 -0600

    generates questions

--- src/app/PassageSearch.tsx
+++ src/app/PassageSearch.tsx
@@ -1,15 +1,32 @@
-export default function PassageSearch() {
-  const data = fetch(`https://api.esv.org/v3/passage/search/?q=John3:16`, {
-    headers: {
-      Authorization: `Token ${process.env.NEXT_PUBLIC_API_KEY}`
-    }
-  }).then((res) => res.json())
+import OpenAI from "openai"
 
-  console.log(data)
+export default async function PassageSearch() {
+  const openai = new OpenAI({ apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY })
+
+  const verses = "Galatians 2"
+  const questions = 8
+
+  const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are a Christian Biblical scholar. Provide a small group discussion guide for the given passages of Scripture."
+      },
+      {
+        role: "user",
+        content: `Create ${questions} small group discussion questions based on ${verses}`
+      }
+    ],
+    store: true
+  })
+
+  console.log(completion.choices[0].message)
 
   return (
     <div>
-      <p>{data}</p>
+      <p>{completion.choices[0].message.content}</p>
     </div>
   )
 }
